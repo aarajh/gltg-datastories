c09ceddb8b40038255005458f7c9b35fa8e5c830
kaveh <kaveh@illinois.edu>
2020-03-24 12:42:12 -0700

    Apply eslint rules

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import slidesMississippiNutrientLoad from './N-P-Loads-Mississippi/slides.html'
-import thumbnailMississippiNutrientLoad from './N-P-Loads-Mississippi/thumbnail.png'
-import thumbnailNutrientLossIllinois from './Nutrient-Loss-Illinois/thumbnail.png'
-import thumbnailExploreData from './Explore-Data-GLTG/thumbnail.png'
-import thumbnailLayers from './Layers/thumbnail.png'
-import thumbnailDataCollection from './Data-Collection/thumbnail.png'
-import thumbnailContinuousMonitoring from './Continuous-Monitoring/thumbnail.png'
-import thumbnailBestManagementPractices from './Best-Management-Practices/thumbnail.png'
-import thumbnailCropScape from './Crop-Scape/thumbnail.png'
-import thumbnailNOAA from './NOAA/thumbnail.png'
-import thumbnailSparrowModel from './Sparrow-Model/thumbnail.png'
+import slidesMississippiNutrientLoad from './N-P-Loads-Mississippi/slides.html';
+import thumbnailMississippiNutrientLoad from './N-P-Loads-Mississippi/thumbnail.png';
+import thumbnailNutrientLossIllinois from './Nutrient-Loss-Illinois/thumbnail.png';
+import thumbnailExploreData from './Explore-Data-GLTG/thumbnail.png';
+import thumbnailLayers from './Layers/thumbnail.png';
+import thumbnailDataCollection from './Data-Collection/thumbnail.png';
+import thumbnailContinuousMonitoring from './Continuous-Monitoring/thumbnail.png';
+import thumbnailBestManagementPractices from './Best-Management-Practices/thumbnail.png';
+import thumbnailCropScape from './Crop-Scape/thumbnail.png';
+import thumbnailNOAA from './NOAA/thumbnail.png';
+import thumbnailSparrowModel from './Sparrow-Model/thumbnail.png';
 
 export default [
     {
@@ -52,4 +52,4 @@ export default [
         title: 'Sparrow Model',
         thumbnail: thumbnailSparrowModel
     }
-]
+];
